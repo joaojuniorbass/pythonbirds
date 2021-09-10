e1456f7c7aee109b759d25b63eb6aac967c92ea8
joao <joaojuniorbass@yahoo.com.br>
2021-09-10 03:04:33 -0300

    Criado um atributo complexo chamado filhos

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,11 +1,14 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=35):
-        self.idade = idade
+    def __init__(self, *filhos, nome = 'João', idade = 38):
         self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
+    def cumprimentar(self, nome):
+        return f"Olá {nome}, eu me chamo {self.nome} e tenho {self.idade} anos de idade"
 
-    def cumprimenttar(self):
-        return f"Olá amigo, me chamo {self.nome}"
 
 if __name__ == "__main__":
-    pessoa = Pessoa('João Carlos Marins')
-    print(pessoa.cumprimenttar())
+    pessoa = Pessoa('Pedro', 'Augusta', 'Josué')
+    print(pessoa.cumprimentar('Anderson do Molejão'))
+    for nome in pessoa.filhos:
+        print(f'Um filho se chama {nome}')
